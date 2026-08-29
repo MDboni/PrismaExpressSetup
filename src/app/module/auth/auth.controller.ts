2bf4bbbaa55bbbc9cb71eb50c1364a75b0f17bfd
Boni Amin Jayed <aminboni048@gmail.com>
2026-08-29 10:36:43 +0600

    ZodErrorHandale

--- src/app/module/auth/auth.controller.ts
+++ src/app/module/auth/auth.controller.ts
@@ -4,10 +4,32 @@ import { catchAsync } from "../../utils/catchAsync";
 import { sendResponse } from "../../utils/sendResponse";
 import type { IRequestUser } from "./auth.interface";
 import { AuthService } from "./auth.service";
+import { z } from "zod";
+
+const PatientRegistration = z.object({
+	name: z.string().min(2).max(100),
+	email: z.email(),
+	password: z.string().min(6).max(100)
+	          .regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/, "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character"),
+	patient: z.object({
+		contactNumber: z.string().optional(),
+		age: z.number().min(0),
+	}),
+});
 
 const registerPatient = catchAsync(async (req: Request, res: Response) => {
-	const payload = req.body;
-	const result = await AuthService.registerPatient(payload);
+	const payload = PatientRegistration.safeParse(req.body);
+
+		
+	if (!payload.success) {
+		let errorMessage = "";
+		payload.error.issues.forEach((error) => {
+			errorMessage += `${error.path.join(".")}: ${error.message}\n`;
+		});
+		throw new Error(errorMessage);
+	}
+
+	const result = await AuthService.registerPatient(payload.data);
 
 	const { accessToken, refreshToken, user, patient } = result;
 

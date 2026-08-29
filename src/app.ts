2d760d51e24a64cf430ccf3f0d5e4f732de3a9fb
Boni Amin Jayed <aminboni048@gmail.com>
2026-08-29 17:52:23 +0600

    asdf

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import express, {
+	NextFunction,
 	type Application,
 	type Request,
 	type Response,
@@ -10,6 +11,7 @@ import config from "./app/config";
 import { globalErrorHandler } from "./app/middleware/globalErrorHandler";
 import { notFound } from "./app/middleware/notFound";
 import { AuthRoutes } from "./app/module/auth/auth.route";
+import { redisClient } from "./app/lib/redis";
 
 const app: Application = express();
 
@@ -28,6 +30,35 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/v1/auth", AuthRoutes);
+
+
+app.get("/test", async (req: Request, res: Response, next : NextFunction) => {
+
+	try {
+
+		// 100000 > 999999 > 1000000
+			const otp = crypto.randomInt(100000, 1000000) // 1, 2, 3, 4, 5, 6,7,8 ,9, 10 => X-11
+		
+			await redisClient.set("forgot-password-otp:patient1@gmail.com", "123456", {
+				expiration : {
+					type : "EX",
+					value : 60
+				}
+			})
+
+		
+
+
+		res.status(httpStatus.OK).json({
+			success: true,
+			message: "Welcome to PH Healthcare System Backend",
+			data : otp
+		});
+	} catch (error) {
+		console.log(error);
+		next(error)
+	}
+})
 
 // Basic route
 app.get("/", async (req: Request, res: Response) => {
